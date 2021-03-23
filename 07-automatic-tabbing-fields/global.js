12fcebaca5c32f100666a29226a06d854a31ec89
MiNTwastaken <nauris.theproducer@gmail.com>
2021-03-23 16:21:55 +0200

    Seventh one

--- 07-automatic-tabbing-fields/global.js
+++ 07-automatic-tabbing-fields/global.js
@@ -1,1 +1,23 @@
-// Your JavaScript goes here.
+window.addEventListener("load", function(){
+
+    var first = document.getElementById('first');
+    var second = document.getElementById('second');
+    var last = document.getElementById('third');
+  
+    first.addEventListener("keyup", function() {
+      numbers = first.value.length;
+      maxlength = parseInt(first.getAttribute("maxlength"));
+      if (numbers === maxlength) {
+        second.focus();
+      }
+    });
+  
+    second.addEventListener("keyup", function() {
+        numbers = second.value.length;
+      maxlength = parseInt(second.getAttribute("maxlength"));
+      if (numbers === maxlength) {
+        last.focus();
+      }
+    });
+  
+  });
