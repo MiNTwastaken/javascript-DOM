7a2410cdb29b23fe19d374f9bed564c8482e9d59
MiNTwastaken <nauris.theproducer@gmail.com>
2021-03-23 16:10:20 +0200

    Sixth one

--- 06-detecting-typed-input/global.js
+++ 06-detecting-typed-input/global.js
@@ -1,1 +1,13 @@
-// Your JavaScript goes here.
+window.addEventListener("load", function(){
+
+    var name = document.getElementById("full_name");
+  
+    name.addEventListener("keyup", function() {
+  
+      var greeting = document.getElementById("greeting");
+  
+      greeting.innerHTML = ("Hello there, " + name.value + "!");
+  
+    });
+  
+  });
