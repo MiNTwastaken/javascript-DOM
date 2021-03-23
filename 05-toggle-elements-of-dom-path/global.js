5c245363a4f6823366f7cabafc66d6196d637236
MiNTwastaken <nauris.theproducer@gmail.com>
2021-03-23 15:55:28 +0200

    Fifth one

--- 05-toggle-elements-of-dom-path/global.js
+++ 05-toggle-elements-of-dom-path/global.js
@@ -1,1 +1,16 @@
-// Your JavaScript goes here.
+window.addEventListener("load", function(){
+    var button = document.getElementById("toggle_button");
+    button.addEventListener("click", function() {
+      var els = document.querySelectorAll("ul.second_five li");
+      for (var i = 0; i < els.length; i++) {
+        if (els[i].style.display === "list-item") {
+          els[i].style.display = "none";
+        }
+        else {
+          els[i].style.display = "list-item";
+        }
+      }
+  
+    });
+  
+  });
